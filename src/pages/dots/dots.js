ac78b204b075561bff3e7e03af86558bc2242a8b
Simon DELAMARRE <sdelamarre@LMACIDN0016.local>
2015-08-28 00:51:38 +0200

    dot game advances easeljs

--- src/pages/dots/dots.js
+++ src/pages/dots/dots.js
@@ -1,6 +1,10 @@
 delete self;
 var self = null;
 function dots(){
+    this._levels = null;
+    this._navigation = {container:null, buttons:null, line:null};
+    
+    
     this.dots = [];
     this.grid_size = {c:3, l:3};
     this.position = {x:0, y:0};
@@ -18,6 +22,12 @@ function dots(){
     this._selected_dots = [];
     this._full_selected_dots = [];
     this.total_dots = 0;
+    
+    this._objectives_container = null;
+    this._objectives_texts = {};
+    this._objectives_dots = null;
+    this._objectives_hits = {};
+    
     this.colors = {
         "yellow":colors.yellow
     }
@@ -37,6 +47,58 @@ dots.prototype.create_interface = function(){
     self = this;
     self.load_level(1);
 }
+dots.prototype.create_objectives = function(){
+    self._objectives_container = self.stage.addChild(new createjs.Container());
+    
+    self._objectives_hits.icon = new createjs.Bitmap("./pages/dots/assets/hit_icon_small.png");
+    self._objectives_hits.icon.x = 70;
+    self._objectives_container.addChild(self._objectives_hits.icon);
+    
+    self._objectives_hits.text = new createjs.Text();
+    self._objectives_hits.text.font = "100 25px Roboto";
+    self._objectives_hits.text.color = "#FFFFFF";
+    self._objectives_hits.text.text = self._total_hits;
+    self._objectives_hits.text.x = 90;
+    self._objectives_hits.text.y = 35;
+    self._objectives_hits.text.width = 100;
+    self._objectives_hits.text.textAlign = "center"; 
+    self._objectives_container.addChild(self._objectives_hits.text);
+    
+    $.each(_.keys(self._objectives), function(i, obk){
+        self._objectives_texts[obk] = {dot:null, text:null, percent:null};
+        self._objectives_texts[obk].dot = new createjs.Shape();
+        self._objectives_texts[obk].dot.graphics.beginFill(colors[obk]).drawCircle(0, 0, 10);
+        self._objectives_texts[obk].dot.y = 15;
+        self._objectives_texts[obk].dot.x = 100 + (80 * (i+1));
+        self._objectives_container.addChild(self._objectives_texts[obk].dot);
+        
+        self._objectives_texts[obk].percent = new createjs.Shape();
+        self._objectives_texts[obk].percent.graphics.setStrokeStyle(3);
+        self._objectives_texts[obk].percent.graphics.beginStroke(colors[obk]);
+        self._objectives_texts[obk].percent.graphics.arc(0,0,16,0,0);
+        self._objectives_texts[obk].percent.y = 15;
+        self._objectives_texts[obk].percent.x = 100 + (80 * (i+1));
+        self._objectives_container.addChild(self._objectives_texts[obk].percent);
+        
+        self._objectives_texts[obk].check = new createjs.Bitmap("./pages/dots/assets/check_small.png");
+        self._objectives_texts[obk].check.y = 0;
+        self._objectives_texts[obk].check.x = 100 + (80 * (i+1)) - 15;
+        self._objectives_texts[obk].check.scaleX = 0;
+        self._objectives_texts[obk].check.scaleY = 0;
+        self._objectives_container.addChild(self._objectives_texts[obk].check);
+        
+        self._objectives_texts[obk].text = new createjs.Text();
+        self._objectives_texts[obk].text.font = "100 15px Roboto";
+        self._objectives_texts[obk].text.color = "#FFFFFF";
+        self._objectives_texts[obk].text.text = "0/"+self._objectives[obk];
+        self._objectives_texts[obk].text.x = 100 + (80 * (i+1));
+        self._objectives_texts[obk].text.y = 45;
+        self._objectives_texts[obk].text.width = 100;
+        self._objectives_texts[obk].text.textAlign = "center"; 
+        self._objectives_container.addChild(self._objectives_texts[obk].text);
+    });
+    
+}
 dots.prototype.init_stage = function(){
     $('#dots_canvas').css({"width":window.innerWidth, "height":window.innerHeight});
     $('#dots_canvas').attr({"width":window.innerWidth, "height":window.innerHeight});
@@ -44,11 +106,7 @@ dots.prototype.init_stage = function(){
     self.stage.autoClear = true;
     self.stage.update();
     createjs.Touch.enable(self.stage);
-    self.stage.addEventListener("stagemousedown", self.stage_down);
-    self.stage.addEventListener("stagemouseup", self.stage_up);
-    
     createjs.Ticker.addEventListener("tick", self.tick);
-    self.create_grid();
 }
 dots.prototype.stage_down = function(e){
     self._is_shape = false;
@@ -66,24 +124,24 @@ dots.prototype.stage_up = function(e){
     }   
 }
 dots.prototype.create_grid = function(){
-    //self._lines = self.stage.addChild(new createjs.Container());
-    self.grid_ground = new createjs.Shape();
-    self.grid_ground.graphics.beginFill("#FFFFFF").drawRoundRect(
-        0,
-        0,
-        (self._grid_size * self.grid_size.c)+self._grid_size, 
-        (self._grid_size * self.grid_size.l)+self._grid_size,
-        10
-    );
-    self.stage.addChild(self.grid_ground);
+    self.stage.addEventListener("stagemousedown", self.stage_down);
+    self.stage.addEventListener("stagemouseup", self.stage_up);
+    
+    self.create_objectives();
     
     self._line = new createjs.Shape();
     self.stage.addChild(self._line);
     
     self.grid = self.stage.addChild(new createjs.Container());
+    //self.grid.y = -window.innerHeight;
+    //TweenMax.to(self.grid, 1, {y:0});
     
-    self.grid_ground.x = self.grid.x = self._line.x = (window.innerWidth/2) - ((self._grid_size * self.grid_size.c)/2) - (self._grid_size/2);
-    self.grid_ground.y = self.grid.y = self._line.y = (window.innerHeight/2) - ((self._grid_size * self.grid_size.l)/2) - (self._grid_size/2);
+    self._objectives_container.x = self.grid.x = self._line.x = (window.innerWidth/2) - ((self._grid_size * self.grid_size.c)/2) - (self._grid_size/2);
+    self.grid.y = self._line.y = (window.innerHeight/2) - ((self._grid_size * self.grid_size.l)/2) - (self._grid_size/2);
+    
+    self._objectives_container.y = (window.innerHeight/2) - ((self._grid_size * self.grid_size.l)/2) - (self._grid_size/2) -50;
+
+    
     var curr = 1;
     var line = 1;
     for(var i=0; i<(self.grid_size.c * self.grid_size.l); i++){
@@ -237,9 +295,11 @@ dots.prototype.check_selected_dots = function(){
         var first_dot = _.findWhere(self.dots, {id:to_remove[0]});
         if(typeof first_dot !== "undefined"){
             if(typeof self._completed_objectives[first_dot.color] !== "undefined"){
-                self._completed_objectives[first_dot.color] += to_remove.length;
+                self._completed_objectives[first_dot.color].count += parseInt(to_remove.length);
+                self._completed_objectives[first_dot.color].percent = (self._completed_objectives[first_dot.color].count * 100) / self._objectives[first_dot.color];
             }else{
-                self._completed_objectives[first_dot.color] = to_remove.length;
+                self._completed_objectives[first_dot.color] = {percent:0, count:0};
+                self._completed_objectives[first_dot.color].count = to_remove.length;
             }
             /* ---- Si on a une suite on joue une anim puis on detruit les dots ---- */
             TweenMax.set($('#colorer'), {backgroundColor:self.colors[first_dot.color]});
@@ -304,7 +364,7 @@ dots.prototype.replace_dots = function(num){
             var child = self.grid.getChildAt(i);
             TweenMax.to(child, .5, {
                 y : child.line * self._grid_size,
-                ease : Power4.easeOut
+                ease : Bounce.easeOut
             });
         }
         setTimeout(function(){
@@ -317,11 +377,15 @@ dots.prototype.replace_dots = function(num){
                     case "win":
                         /* --- ouverture de la pop up winner --- */
                         /* --- Choix 1 retour au menu principal --- */
+                        self.destroy_grid();
+                        self.open_nav();
                         /* --- Choix 2 Level Suivant --- */
                         break;
                     case "lose":
                         /* --- ouverture de la pop up loser --- */
                         /* --- Choix 1 retour au menu principal --- */
+                        self.destroy_grid();
+                        self.open_nav();
                         /* --- Choix 2 Rejouer --- */
                         break;
                 }
@@ -334,8 +398,26 @@ dots.prototype.check_game_state = function(callBack){
     var _objectives_infos = {total:_.keys(self._objectives).length, completed:0};
     /* --- On check si les objectifs sont remplis et on met à jour la partie objectifs ----- */
     $.each(_.keys(self._objectives), function(i, obk){
-        if(self._completed_objectives[obk] >= self._objectives[obk]){
+        if(self._completed_objectives[obk].count >= self._objectives[obk]){
+            self._objectives_texts[obk].percent.graphics.clear();
+            self._objectives_texts[obk].percent.graphics.setStrokeStyle(3);
+            self._objectives_texts[obk].percent.graphics.beginStroke(colors[obk]);
+            self._objectives_texts[obk].percent.graphics.arc(0,0,16,0,6);
+            
+            self._objectives_texts[obk].text.text = self._objectives[obk];
+            
+            TweenMax.to(self._objectives_texts[obk].dot, .2, {scaleX:2, scaleY:2, ease:Back.easeOut});
+            TweenMax.to(self._objectives_texts[obk].check, .2, {scaleX:1, scaleY:1, delay:.1, ease:Back.easeOut});
+            
             _objectives_infos.completed++;
+        }else{
+            self._objectives_texts[obk].percent.graphics.clear();
+            self._objectives_texts[obk].percent.graphics.setStrokeStyle(3);
+            self._objectives_texts[obk].percent.graphics.beginStroke(colors[obk]);
+            var angle = (6 * self._completed_objectives[obk].percent)/100;
+            self._objectives_texts[obk].percent.graphics.arc(0,0,16,0,angle);
+            
+            self._objectives_texts[obk].text.text = self._completed_objectives[obk].count+"/"+self._objectives[obk];
         }
     });
     /* --- si les objectifs sont remplis on gagne ---- */
@@ -345,6 +427,7 @@ dots.prototype.check_game_state = function(callBack){
         status.objectifs = true;
     }
     /* --- On check s'il reste des hits et on met à jour les hits ------- */
+    self._objectives_hits.text.text = self._total_hits - self._hits;
     if(self._total_hits - self._hits <= 0){
         if(status.objectifs === false){
             status.label = "lose";
@@ -433,11 +516,12 @@ dots.prototype.set_dot_color = function(target, color){
     target.graphics.beginFill(color).drawCircle(0, 0, self._dot_size);
 }
 dots.prototype.load_level = function(lvl){
-    if(self.grid_ground){
-        TweenMax.to(self.grid_ground, .5, {scaleX:0, scaleY:0});
+    self.init_stage();
+    if(self.grid){
+        //TweenMax.to(self.grid_ground, .5, {scaleX:0, scaleY:0});
         TweenMax.to(self._line, .5, {scaleX:0, scaleY:0});
         TweenMax.to(self.grid, .5, {scaleX:0, scaleY:0});
-        self.stage.removeChild(self.grid_ground);
+        //self.stage.removeChild(self.grid_ground);
         self.stage.removeChild(self._line);
         self.stage.removeChild(self.grid);
     }
@@ -446,22 +530,81 @@ dots.prototype.load_level = function(lvl){
         url: './pages/dots/levels/level_'+lvl+'.json',
         data: {},
         success: function(datas){
-            self._grid_size = datas._grid_size;
-            self.colors     = {};
-            $.each(datas.colors, function(i,cl){
-                self.colors[datas.colors[i]] = colors[datas.colors[i]];
-            });
-            self.dot_size       = datas.dot_size;
-            self.grid_size      = datas.grid_size;
-            self._total_hits    = datas.hits;
-            self._hits          = 0;
-            self._objectives     = datas.objectives;
-            self._completed_objectives     = {};
-            $.each(_.keys(datas.objectives), function(o, obk){
-                self._completed_objectives[obk] = 0;
-            });
-            self.init_stage();
+            self._levels = datas;
+            self.build_navigation();
         }
+    });
+}
+dots.prototype.select_level = function(id){
+    var datas = self._levels[id];
+    self._grid_size = datas._grid_size;
+    self.colors     = {};
+    $.each(datas.colors, function(i,cl){
+        self.colors[datas.colors[i]] = colors[datas.colors[i]];
+    });
+    self._dot_size       = datas.dot_size;
+    self.grid_size      = datas.grid_size;
+    self._total_hits    = datas.hits;
+    self._hits          = 0;
+    self._objectives     = datas.objectives;
+    self._completed_objectives     = {};
+    $.each(_.keys(datas.objectives), function(o, obk){
+        self._completed_objectives[obk] = {count:0, percent:0};
+    });
+    TweenMax.to($('.app_content'), .5, {backgroundColor:colors[datas.color]});
+    TweenMax.to($('#backbutton'), .5, {backgroundColor:colors[datas.color]});
+    TweenMax.to(self._navigation.container, 1, {y:window.innerHeight, onComplete : function(){
+        self.create_grid();
+    }});
+}
+dots.prototype.destroy_grid = function(){
+    var l = self.grid.getNumChildren();
+    for (var i=0; i<l; i++) {
+        self.dots = _.reject(self.dots, {id:self.grid.getChildAt(i).id});
+        //self.grid.removeChild(self.grid.getChildAt(i));
+    }
+    self.stage.removeChild(self.grid);
+    self.stage.removeChild(self._objectives_container);
+}
+dots.prototype.open_nav = function(){
+    TweenMax.to(self._navigation.container, .5, {y:0});
+}
+dots.prototype.build_navigation = function(){
+    self._navigation = {container:null, buttons:{}, line:null};
+    self._navigation.container = self.stage.addChild(new createjs.Container());
+    self._navigation.line = new createjs.Shape();
+    self._navigation.line.graphics.setStrokeStyle(40);
+    self._navigation.line.graphics.beginStroke("#FFF");
+    self._navigation.line.graphics.moveTo(260, (window.innerHeight / 2) - 25);
+    self._navigation.line.graphics.lineTo((100 + (160 * _.keys(self._levels).length)), (window.innerHeight / 2) - 25);
+    self._navigation.container.addChild(self._navigation.line);
+    
+    $.each(_.keys(self._levels), function(i, lvl){
+        self._navigation.buttons[lvl] = {dot:null, text:null, percent:null};
+        self._navigation.buttons[lvl].dot = new createjs.Shape();
+        self._navigation.buttons[lvl].dot.graphics.setStrokeStyle(20);
+        self._navigation.buttons[lvl].dot.graphics.beginStroke("#FFF");
+        self._navigation.buttons[lvl].dot.graphics.beginFill(colors[self._levels[lvl].color]).drawCircle(0, 0, 50);
+        self._navigation.buttons[lvl].dot.y = (window.innerHeight / 2) - 25;
+        self._navigation.buttons[lvl].dot.x = 100 + (160 * (i+1));
+        self._navigation.buttons[lvl].dot.id = lvl;
+        
+        
+        self._navigation.buttons[lvl].text = new createjs.Text();
+        self._navigation.buttons[lvl].text.font = "700 50px Roboto";
+        self._navigation.buttons[lvl].text.color = "#FFFFFF";
+        self._navigation.buttons[lvl].text.text = lvl;
+        self._navigation.buttons[lvl].text.x = 100 + (160 * (i+1));
+        self._navigation.buttons[lvl].text.y = (window.innerHeight / 2) - 55;
+        self._navigation.buttons[lvl].text.width = 100;
+        self._navigation.buttons[lvl].text.textAlign = "center"; 
+        
+        self._navigation.container.addChild(self._navigation.buttons[lvl].dot);
+        self._navigation.container.addChild(self._navigation.buttons[lvl].text);
+        
+        self._navigation.buttons[lvl].dot.addEventListener("click", function(e){
+            self.select_level(e.currentTarget.id);
+        });
     });
 }
 dots.prototype.pause = function(){
